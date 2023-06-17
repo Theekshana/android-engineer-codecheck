a7a21c5c45f0e1e53baf51d7fcb07411aaad0297
Theekshana <tvimukthi.m@gmail.com>
2023-06-17 14:00:54 +0900

    Create retrofit instance

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/di/NetworkModule.kt
@@ -0,0 +1,68 @@
+package jp.co.yumemi.android.code_check.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jp.co.yumemi.android.code_check.constants.Constants
+import okhttp3.OkHttpClient
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    /**
+     *Provides the base URL for API calls.
+     */
+    @Singleton
+    @Provides
+    fun provideBaseUrl(): String {
+        return Constants.BASE_URL
+    }
+
+    /**
+     * Provides the converter factory for JSON serialization.
+     */
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): Converter.Factory {
+        return GsonConverterFactory.create()
+    }
+
+    /**
+     * Provides the OkHttpClient for network requests.
+     */
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+        return okHttpClient.build()
+    }
+
+    /**
+     * Provides the Retrofit instance.
+     *
+     * @param baseUrl The base URL for API calls.
+     * @param okHttpClient The OkHttpClient instance for network requests.
+     * @param converterFactory The Converter.Factory for JSON
+     */
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        baseUrl: String,
+        okHttpClient: OkHttpClient,
+        converterFactory: Converter.Factory,
+    ): Retrofit {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(converterFactory)
+            .client(okHttpClient)
+
+        return retrofit.build()
+    }
+
+}
