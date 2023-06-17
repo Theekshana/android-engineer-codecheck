72530fd55ad25df3fb24bdd1c78cc198507e8336
Theekshana <tvimukthi.m@gmail.com>
2023-06-17 16:29:39 +0900

    Add viemodel class

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubRepository.kt
@@ -1,8 +1,31 @@
 package jp.co.yumemi.android.code_check.repository
 
+import jp.co.yumemi.android.code_check.model.ServerResponse
 import jp.co.yumemi.android.code_check.network.GithubApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GithubRepository @Inject constructor(
     private val githubApiService: GithubApiService) {
+
+    /**
+     * Get GitHub account information from the data source.
+     * @param q The query string to search for repositories.
+     * @return The server response, or null if the request was unsuccessful.
+     */
+    suspend fun getGitHutAccountsFromDataSource(q: String): ServerResponse? {
+        return withContext(Dispatchers.IO) {
+            return@withContext getResponseFromRemoteService(q)
+        }
+    }
+
+    private suspend fun getResponseFromRemoteService(q: String): ServerResponse? {
+        val response = githubApiService.getRepositories(q)
+        if (response.isSuccessful) {
+            return response.body()
+        }
+        return null
+    }
+
 }
