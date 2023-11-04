418c52b1ae691ff9b0596e692f8c44f601c832aa
Theekshana <tvimukthi.m@gmail.com>
2023-11-04 23:32:22 +0900

    Update gradle

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/model/UIState.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/model/UIState.kt
@@ -4,6 +4,6 @@ sealed class UIState<out T> {
 
     data class Success<out T>(val data: T) : UIState<T>()
     data class Error(val message: String) : UIState<Nothing>()
-    object Loading : UIState<Nothing>()
+    data object Loading : UIState<Nothing>()
 
 }
